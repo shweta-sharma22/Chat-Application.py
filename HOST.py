59b1c4bb846d0c66facd521bdc1fb51759927065
AbhipreyaGhosh <ghoshabhipreya@gmail.com>
2023-07-08 15:56:58 +0530

    new_multithreading_branch

--- HOST.py
+++ HOST.py
@@ -1,24 +1,58 @@
+# import socket
+# import sys
+# import time
+
+# s=socket.socket()
+# host=socket.gethostname()
+# print("Your connection server is starting on", host)
+# port=1025
+
+# s.bind((host, port))
+# print("Server is ready to accept")
+
+# s.listen(1)
+# connection, addr=s.accept()
+# print("Connection established to address: ", addr)
+
+# while 1:
+#     message = input(str("Enter your message:"))
+#     message=message.encode()
+#     connection.send(message)
+
+#     incoming_message=connection.recv(1024)
+#     incoming_message=incoming_message.decode()
+#     print("Reply:", incoming_message)
+
+
 import socket
 import sys
 import time
+import threading
 
-s=socket.socket()
-host=socket.gethostname()
-print("Your connection server is starting on", host)
-port=1025
+def receive_messages(s):
+    while True:
+        incoming_message = s.recv(1024)
+        incoming_message = incoming_message.decode()
+        if not incoming_message:
+            break
+        print("Reply ->", incoming_message)
 
-s.bind((host, port))
-print("Server is ready to accept")
+host = input("Please enter the host you want to connect to: ")
+port = 1025
 
-s.listen(1)
-connection, addr=s.accept()
-print("Connection established to address: ", addr)
+try:
+    s = socket.socket()
+    s.connect((host, port))
+    print("Connected to Host")
+except:
+    print("Connection Failed")
+    sys.exit()
 
-while 1:
-    message = input(str("Enter your message:"))
-    message=message.encode()
-    connection.send(message)
+# Start a thread to receive messages from the server
+receive_thread = threading.Thread(target=receive_messages, args=(s,))
+receive_thread.start()
 
-    incoming_message=connection.recv(1024)
-    incoming_message=incoming_message.decode()
-    print("Reply:", incoming_message)
+while True:
+    message = input("Enter your Message: ")
+    message = message.encode()
+    s.send(message)
--- SERVER.py
+++ SERVER.py
@@ -1,22 +1,59 @@
+# import socket
+# import sys
+# import time
+
+# s=socket.socket()
+# host=input(str("Please enter the host you want to connect to: "))
+# port=1025
+
+# try:
+#     s.connect((host, port))
+#     print("Connected to Host")
+# except:
+#     print("Connection Failed")
+
+# while 1:
+#     incoming_message = s.recv(1024)
+#     incoming_message = incoming_message.decode()
+#     print("Reply-> " , incoming_message)
+
+# message=input(str("Enter your Message-> "))
+# message=message.encode()
+# s.send(message)
+
 import socket
 import sys
 import time
+import threading
 
-s=socket.socket()
-host=input(str("Please enter the host you want to connect to: "))
-port=1025
+def client_thread(connection, addr):
+    print("Connection established with:", addr)
 
-try:
-    s.connect((host, port))
-    print("Connected to Host")
-except:
-    print("Connection Failed")
+    while True:
+        message = connection.recv(1024)
+        if not message:
+            break
+        incoming_message = message.decode()
+        print("Received from", addr, ":", incoming_message)
 
-while 1:
-    incoming_message = s.recv(1024)
-    incoming_message = incoming_message.decode()
-    print("Reply-> " , incoming_message)
+        reply_message = "Message received from " + str(addr) + ": " + incoming_message
+        connection.send(reply_message.encode())
 
-message=input(str("Enter your Message-> "))
-message=message.encode()
-s.send(message)
+    connection.close()
+    print("Connection closed with:", addr)
+
+s = socket.socket()
+host = socket.gethostname()
+print("Your connection server is starting on", host)
+port = 1025
+
+s.bind((host, port))
+print("Server is ready to accept connections.")
+
+s.listen(5)
+print("Listening for incoming connections...")
+
+while True:
+    connection, addr = s.accept()
+    thread = threading.Thread(target=client_thread, args=(connection, addr))
+    thread.start()
